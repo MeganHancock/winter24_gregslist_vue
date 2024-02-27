21d12a2c798763dbde46d1ea03940b517f292925
MeganHancock <mgnhnck@gmail.com>
2024-02-27 14:59:03 -0700

    set up houses page, can get there via url

---
+++ src/pages/HousesPage.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container-fluid">
+        <section class="row">
+            <div class="col-12">
+                <h1 class="m-3">
+                    Houses
+                </h1>
+            </div>
+        </section>
+    </div>
+</template>
+
+
+<script>
+export default {
+
+    setup() {
+
+
+        return {
+
+
+        }
+    }
+}
+
+
+</script>
+
+
+<style lang="scss" scoped></style>
