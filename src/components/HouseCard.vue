849523f50aaa980a8c52b5f7531548afe653dba9
MeganHancock <mgnhnck@gmail.com>
2024-02-27 16:14:12 -0700

    separated house card

---
+++ src/components/HouseCard.vue
@@ -0,0 +1,46 @@
+<template>
+    <img :src="houseProp.imgUrl" class="img-fluid"
+        :alt="'A house with ' + houseProp.bedrooms + ' bedrooms and ' + houseProp.bathrooms + ' bathrooms'">
+    <div class="house-caption">
+        <p class="fs-5 mb-1 ms-1">{{ houseProp.bedrooms }} bedroom {{ houseProp.bathrooms }} bathroom house</p>
+    </div>
+</template>
+
+
+<script>
+
+import { computed } from 'vue';
+import { AppState } from '../AppState.js';
+import { House } from '../models/House.js';
+
+export default {
+    props: {
+        houseProp: { type: House, required: true }
+    },
+
+    setup() {
+        return {}
+    }
+}
+</script>
+
+
+<style lang="scss" scoped>
+img {
+    height: 40vh;
+    width: 40vh;
+    border-radius: 16px;
+    object-fit: cover;
+    object-position: center;
+    box-shadow: 2px 3px 10px black;
+}
+
+
+.house-caption {
+    position: absolute;
+    bottom: 0;
+    color: white;
+    text-shadow: 1px 1px 3px black;
+    font-weight: bold;
+}
+</style>
--- src/pages/HousesPage.vue
+++ src/pages/HousesPage.vue
@@ -9,11 +9,7 @@
 
         <section class="row justify-content-center">
             <div v-for="house in houses" :key="house.id" class="col-md-4 mb-3 house-card">
-                <img :src="house.imgUrl" class="img-fluid"
-                    :alt="'A house with ' + house.bedrooms + ' bedrooms and ' + house.bathrooms + ' bathrooms'">
-                <div class="house-caption">
-                    <p class="fs-5 mb-1 ms-1">{{ house.bedrooms }} bedroom {{ house.bathrooms }} bathroom house</p>
-                </div>
+                <HouseCard :houseProp="house" />
             </div>
         </section>
 
@@ -28,30 +24,26 @@ import Pop from '../utils/Pop.js';
 import { housesService } from '../services/HousesService.js'
 import { logger } from '../utils/Logger.js';
 import { AppState } from '../AppState.js'
+import HouseCard from '../components/HouseCard.vue';
 
 export default {
-
     setup() {
-
         async function getHouses() {
             try {
-                await housesService.getHouses()
-            } catch (error) {
-                Pop.error(error)
+                await housesService.getHouses();
+            }
+            catch (error) {
+                Pop.error(error);
             }
         }
-
         onMounted(() => {
-            logger.log('page is mounted')
-            getHouses()
-        })
-
+            getHouses();
+        });
         return {
             houses: computed(() => AppState.houses)
-
-        }
-
-    }
+        };
+    },
+    components: { HouseCard }
 }
 
 
@@ -59,24 +51,7 @@ export default {
 
 
 <style lang="scss" scoped>
-img {
-    height: 40vh;
-    width: 40vh;
-    border-radius: 16px;
-    object-fit: cover;
-    object-position: center;
-    box-shadow: 2px 3px 10px black;
-}
-
 .house-card {
     position: relative;
-}
-
-.house-caption {
-    position: absolute;
-    bottom: 0;
-    color: white;
-    text-shadow: 1px 1px 3px black;
-    font-weight: bold;
 }
 </style>
