dadd332af4e9d64ab4aeedf9caa7afb3f2025901
MeganHancock <mgnhnck@gmail.com>
2024-02-27 15:23:42 -0700

    logging mapped houses

--- src/services/HousesService.js
+++ src/services/HousesService.js
@@ -1,3 +1,5 @@
+import { House } from "../models/House.js"
+import { AppState } from "../AppState.js"
 import { logger } from "../utils/Logger.js"
 import { api } from "./AxiosService.js"
 
@@ -6,7 +8,9 @@ class HousesService {
     async getHouses() {
         const response = await api.get('api/houses')
         logger.log('got houses', response.data)
-
+        const houses = response.data.map(housePOJO => new House(housePOJO))
+        AppState.houses = houses
+        logger.log('new Houses mapped', houses)
     }
 }
 
