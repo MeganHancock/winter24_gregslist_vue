804ac0cc7c28607a8258193ef05ae295ff8cfa51
MeganHancock <mgnhnck@gmail.com>
2024-02-27 17:20:57 -0700

    house details page

--- src/pages/HouseDetailsPage.vue
+++ src/pages/HouseDetailsPage.vue
@@ -1,5 +1,27 @@
 <template>
-    <h1>car deets</h1>
+    <div class="container">
+        <section class="row">
+            <div class="col-12">
+                <h1 class="my-3">House Details</h1>
+            </div>
+        </section>
+
+        <section v-if="house" class="row">
+            <div class="col-12 text-center">
+                <img class="rounded-3 m-3" :src="house.imgUrl"
+                    :alt="'A house with ' + house.bedrooms + ' bedrooms and ' + house.bathrooms + ' bathrooms'">
+                <div class="p-3 d-flex justify-content-between">
+                    <p>Bedrooms: {{ house.bedrooms }}</p>
+                    <p>Bathrooms: {{ house.bathrooms }}</p>
+                    <p>Levels: {{ house.levels }}</p>
+                </div>
+                <h1>${{ house.price }}</h1>
+
+                <p>Listed by {{ house.creator.name }}</p>
+                <p>At {{ house.createdAt }}</p>
+            </div>
+        </section>
+    </div>
 </template>
 
 
@@ -7,8 +29,9 @@
 import { useRoute } from 'vue-router';
 import Pop from '../utils/Pop.js';
 import { housesService } from '../services/HousesService.js';
-import { onMounted } from 'vue';
+import { computed, onMounted } from 'vue';
 import { logger } from '../utils/Logger.js';
+import { AppState } from '../AppState.js'
 
 export default {
     setup() {
@@ -31,10 +54,21 @@ export default {
             getHouseById()
         })
 
-        return {}
+        return {
+            house: computed(() => AppState.activeHouse)
+        }
     }
 }
 </script>
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+img {
+    height: 50vh;
+    width: 50vh;
+    object-fit: cover;
+    object-position: center;
+    box-shadow: 2px 3px 10px black;
+
+}
+</style>
